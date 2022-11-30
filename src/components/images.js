fc7284c4ffbb2d10c586e6b1e81c67756d317e2b
David J Crockett <crockett.davidj@gmail.com>
2022-11-30 14:13:13 -0800

    imported images for cards to images.js

---
+++ src/components/images.js
@@ -0,0 +1,15 @@
+import pikachu from "../images/pikachu.png";
+import meowth from "../images/meowth.png";
+import jigglypuff from "../images/jigglypuff.png";
+import marrill from "../images/marrill.png";
+import hoothoot from "../images/hoothoot.png";
+import cacnea from "../images/cacnea.png";
+
+export const cardImages = [
+    { src: pikachu, matched: false },
+    { src: meowth, matched: false },
+    { src: jigglypuff, matched: false },
+    { src: marrill, matched: false },
+    { src: hoothoot, matched: false },
+    { src: cacnea, matched: false }
+]
