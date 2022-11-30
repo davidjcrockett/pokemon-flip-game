1d6a95135ea3b5086497025bc025a90f6c82373c
David J Crockett <crockett.davidj@gmail.com>
2022-11-30 15:57:48 -0800

    Created a shuffle function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import { cardImages } from './components/images';
 
-function App() {
+function App () {
+//need to pass through cards and turns state variables later in my return
+  const [ cards, setCards ] = useState ([]);
+  const [ turns, setTurns ] = useState(0);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+    .sort(() => Math.random() - 0.5)
+    .map((card) => ({...card, id: Math.random ()
+    }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
   return (
     <div>
       Hello, World!
