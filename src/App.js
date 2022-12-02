29ad9cd4ffcee5f93d139443fa9ecb580fab180c
David J Crockett <crockett.davidj@gmail.com>
2022-12-01 23:01:38 -0800

    Imported Header & included shuffle function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import { cardImages } from './components/images';
+import Header from './components/header'
 
 function App () {
 //need to pass through cards and turns state variables later in my return
@@ -23,7 +24,7 @@ function App () {
 
   return (
     <div>
-      Hello, World!
+      <Header turns={turns} onShuffle={shuffleCards} />
     </div>
   );
 }
